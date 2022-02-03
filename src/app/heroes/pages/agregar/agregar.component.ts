ef7ab1874c17d91571d38cb776128bd197d8d77c
Santiago <santtiago49@gmail.com>
2022-02-03 13:21:27 -0300

    Editar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
 
 @Component({
   selector: 'app-agregar',
@@ -34,9 +38,37 @@ export class AgregarComponent implements OnInit {
     alt_img: '',
   }
 
-  constructor() { }
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router ){ }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroePorId( id ))
+      )
+      .subscribe( heroe => this.heroe = heroe)
+      
+  }
+
+
+  guardar(){
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if (this.heroe.id){
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe( resp => console.log('Actualizando', resp))
+    } else {
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe( 
+        heroe => this.router.navigate(['/heroes/editar', heroe.id ])
+       )
+    }
+    
+
   }
 
 }
