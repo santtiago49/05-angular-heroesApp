5c7dbdf872e5810b28423f23d04d3fdc1dd20d27
Santiago <santtiago49@gmail.com>
2022-02-04 20:42:11 -0300

    mostrar informacion del usuario activo

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +10,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor( private router: Router ) { }
+  constructor( private authService: AuthService, 
+               private router: Router, ) { }
 
   login(){
     
     // ir al backend
     // usuario
+    this.authService.login()
+      .subscribe( resp => {
+        console.log(resp.usuario);
+        if( resp.id ){
+          this.router.navigate(['/heroes/listado'])
+        }
+      });
+
+      }
 
 
-    this.router.navigate(['/heroes/listado'])
+
+
+    // this.router.navigate(['/heroes/listado'])
 
   }
-
-}
